bde2185afb3cd05df5fbea3e1409759307e6b78e
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2026-06-01 15:41:17 +0300

    compat: define XtOffsetOf via offsetof for PHP 8.6+

--- common.h
+++ common.h
@@ -30,6 +30,9 @@
 #include <zend_smart_str.h>
 #include <zend_smart_string.h>
 
+#ifndef XtOffsetOf
+# define XtOffsetOf(type, field) offsetof(type, field)
+#endif
 #define PHPREDIS_GET_OBJECT(class_entry, o) (class_entry *)((char *)o - XtOffsetOf(class_entry, std))
 #define PHPREDIS_ZVAL_GET_OBJECT(class_entry, z) PHPREDIS_GET_OBJECT(class_entry, Z_OBJ_P(z))
 
